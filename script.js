b2001831feba2858955dc35e3fdd32f7dd2a11a8
yahayamagaji129-maker <yahayamagaji129@gmail.com>
2026-07-17 10:17:12 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+// Welcome message
+console.log("Welcome to Yahaya Magaji's Portfolio!");
+
+// Show welcome alert when page loads
+window.onload = function () {
+    alert("Welcome to Yahaya Magaji's Portfolio Website!");
+};
